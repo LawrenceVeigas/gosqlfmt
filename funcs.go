e36ed7378efdbfd5cb27269979d9866747c8c48a
lawrenceveigas <lancev.veigas3@gmail.com>
2024-01-25 17:55:48 +0530

    function to parse brackets

--- funcs.go
+++ funcs.go
@@ -42,94 +42,45 @@ func CleanQuery(query string) string {
 	return query
 }
 
+func findPos(text string, openPos int) int {
+	closePos := openPos
+	counter := 1
+	for {
+		if counter == 0 {
+			break
+		}
+
+		closePos += 1
+
+		if closePos > len(text) {
+			panic("No corresponding bracket found")
+		}
+
+		c := text[closePos]
+		if c == '(' {
+			counter++
+		} else if c == ')' {
+			counter--
+		}
+	}
+
+	return closePos
+}
+
 func ReplaceBrackets(query string) string {
+
 	var openBracketIndex []int
-	var tempCloseBracketIndex []int
+	var closeBracketIndex []int
 
 	openBracketIndices := regexp.MustCompile(`\(`).FindAllStringIndex(query, -1)
-	closeBracketIndices := regexp.MustCompile(`\)`).FindAllStringIndex(query, -1)
-
-	fmt.Printf("Opening bracket indices (pre-processing):\n%v\n", openBracketIndices)
-	fmt.Printf("Closing bracket indices (pre-processing):\n%v\n", closeBracketIndices)
 
 	for i := range openBracketIndices {
 		openBracketIndex = append(openBracketIndex, openBracketIndices[i][1])
-	}
-	for i := range closeBracketIndices {
-		tempCloseBracketIndex = append(tempCloseBracketIndex, closeBracketIndices[i][0])
+		closeBracketIndex = append(closeBracketIndex, findPos(query, openBracketIndex[i]))
 	}
 
-	closeBracketIndex := make([]int, len(tempCloseBracketIndex))
-	unused := make([]bool, len(tempCloseBracketIndex))
-	// unused flag for the last index will always be true
-	unused[len(unused)-1] = true
-	// arrange bracket indices
-	for i := range openBracketIndex {
-		j := i + 1
-		if j <= len(openBracketIndex)-1 {
-			for k := range tempCloseBracketIndex {
-				v := tempCloseBracketIndex[k]
-				if v > openBracketIndex[i] && v < openBracketIndex[j] {
-					closeBracketIndex[i] = v
-					unused[k] = false
-					break
-				}
-			}
-			unused[i] = true
-
-		} else if j == len(openBracketIndex) {
-			fmt.Printf("Unused array:%v\n", unused)
-			minDistance := tempCloseBracketIndex[len(tempCloseBracketIndex)-1]
-			index := tempCloseBracketIndex[len(tempCloseBracketIndex)-1]
-			for n := range tempCloseBracketIndex {
-				fmt.Printf("OpenBracketIndex[%v]\t:%v\n", i, openBracketIndex[i])
-				v := unused[n]
-				if v {
-					if openBracketIndex[i] < tempCloseBracketIndex[n] {
-						fmt.Printf("TempCloseBracketIndex[%v]\t:%v\n", n, tempCloseBracketIndex[n])
-						min := tempCloseBracketIndex[n] - openBracketIndex[i]
-						if min < minDistance {
-							minDistance = min
-							index = n
-						}
-					}
-				}
-			}
-			closeBracketIndex[i] = tempCloseBracketIndex[index]
-			unused[index] = false
-
-			// pase closing brackets
-			// fmt.Printf("Unused indexes\n%v\n", unused)
-			// fmt.Printf("Temp close bracket index\n%v\n", tempCloseBracketIndex)
-			// fmt.Printf("Close bracket indexes\n%v\n", closeBracketIndex)
-			// // fmt.Printf("Close bracket index[0]\n%v\n", closeBracketIndex[0] == 0)
-			// // fmt.Printf("Temp Close bracket index[0]\n%v\n", tempCloseBracketIndex[5])
-			// // fill nil/0 positions in reverse order
-			// for l := len(tempCloseBracketIndex) - 1; l >= 0; l-- {
-			// 	v := unused[l]
-			//
-			// 	fmt.Println(l)
-			// 	fmt.Printf("Unused flag %v for %v\n\n", v, tempCloseBracketIndex[l])
-			// 	if v {
-			// 		// fmt.Printf("Unused flag %v for %v\n\n", v, tempCloseBracketIndex[l])
-			// 		for m := range closeBracketIndex {
-			// 			x := closeBracketIndex[m]
-			// 			if x == 0 {
-			// 				fmt.Printf("inserting %v into index %v\n\n", tempCloseBracketIndex[l], m)
-			// 				closeBracketIndex[m] = tempCloseBracketIndex[l]
-			// 				fmt.Printf("close bracket index after insertion\n%v\n", closeBracketIndex)
-			// 				break
-			// 			}
-			// 		}
-			// 	}
-			//
-			// }
-		}
-
-	}
-
-	fmt.Printf("Opening bracket indices (post-processing)\n%v\n", openBracketIndex)
-	fmt.Printf("Closing bracket indices (post-processing)\n%v\n", closeBracketIndex)
+	fmt.Printf("Opening bracket indices:\t%v\n", openBracketIndex)
+	fmt.Printf("Closing bracket indices:\t%v\n", closeBracketIndex)
 
 	swapopen := make([]int, len(openBracketIndex))
 	copy(swapopen, openBracketIndex)
@@ -150,8 +101,8 @@ func ReplaceBrackets(query string) string {
 		swapclose[i] = closeBracketIndex[sortindex[i]]
 	}
 
-	fmt.Println(swapopen)
-	fmt.Println(swapclose)
+	fmt.Printf("Sorted open:\t%v\n", swapopen)
+	fmt.Printf("Sorted close:\t%v\n", swapclose)
 
 	returnquery := query
 
