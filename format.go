ef858d397ed043b29680a8734a0153a02c4e9184
lawrenceveigas <lancev.veigas3@gmail.com>
2024-01-26 20:26:57 +0530

    added logger

--- format.go
+++ format.go
@@ -1,33 +1,50 @@
 package gosqlfmt
 
 import (
-	"fmt"
+	"os"
 	"regexp"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
+
+func init() {
+	log.SetFormatter(&log.TextFormatter{
+		// TimestampFormat: "2006-01-02 15:04:05",
+		TimestampFormat: "15:04:05",
+	})
+
+	log.SetOutput(os.Stdout)
+
+	log.SetLevel(log.DebugLevel)
+	log.SetReportCaller(true)
+}
 
 func matcher(query string) string {
 	match, err := regexp.Match(`(?i)^set`, []byte(query))
-	if err!=nil {
-		fmt.Println(err)
+	if err != nil {
+		log.Error(err)
 	}
 	if match {
+		log.Info("Matched SET")
 		return query
 	}
 
 	match, err = regexp.Match(`(?i)^select`, []byte(query))
-	if err!=nil {
-		fmt.Println(err)
+	if err != nil {
+		log.Error(err)
 	}
 	if match {
+		log.Info("Matched SELECT")
 		return FormatSelect(query)
 	}
 
 	match, err = regexp.Match(`(?i)^with`, []byte(query))
-	if err!=nil {
-		fmt.Println(err)
+	if err != nil {
+		log.Error(err)
 	}
 	if match {
+		log.Info("Matched CTE")
 		return FormatCTE(query)
 	}
 
@@ -38,12 +55,12 @@ func FormatQuery(fileName string) string {
 
 	queries := GetQuery(fileName)
 
-	var finalQuery string 
+	var finalQuery string
 	for i := range queries {
 		query := matcher(CleanQuery(queries[i]))
 
-		if len(finalQuery)>0 {
-			finalQuery = finalQuery+";\n\n"+strings.TrimSpace(query)
+		if len(finalQuery) > 0 {
+			finalQuery = finalQuery + ";\n\n" + strings.TrimSpace(query)
 		} else {
 			finalQuery = strings.TrimSpace(query)
 		}
