55b56f297b0968e83d8a9456fdaecf3e9d7ff657
lawrenceveigas <lancev.veigas3@gmail.com>
2024-01-26 20:27:50 +0530

    used unwrap function to format query within brackets

--- funcs.go
+++ funcs.go
@@ -1,10 +1,11 @@
 package gosqlfmt
 
 import (
-	"fmt"
 	"regexp"
 	"sort"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // map of everything within ()
@@ -20,7 +21,7 @@ var (
 
 	fromtables  = regexp.MustCompile(`(?i)from(.*)(where|group by|order by|having|qualify)(.*)`)
 	fromtables2 = regexp.MustCompile(`(?i)from(.*)`)
-	fmttables   = regexp.MustCompile(`(?i)(^[a-zA-Z][a-zA-Z\._0-9]*[\s]*[a-zA-Z0-9]*)\s((left|right|full|join|lateral)*(.*))`)
+	fmttables   = regexp.MustCompile(`(?i)(^[a-zA-Z][a-zA-Z\._0-9]*[\s]*[a-zA-Z0-9]*)\s*((left|right|full|join|lateral)*(.*))`)
 	joins_etc   = regexp.MustCompile(`(?i)(lateral|left|right|full)`)
 
 	wherecond  = regexp.MustCompile(`(?i)where(.*)(group by|order by|having|qualify)`)
@@ -79,8 +80,8 @@ func ReplaceBrackets(query string) string {
 		closeBracketIndex = append(closeBracketIndex, findPos(query, openBracketIndex[i]))
 	}
 
-	fmt.Printf("Opening bracket indices:\t%v\n", openBracketIndex)
-	fmt.Printf("Closing bracket indices:\t%v\n", closeBracketIndex)
+	log.Debugf("Opening bracket indices:\t%v\n", openBracketIndex)
+	log.Debugf("Closing bracket indices:\t%v\n", closeBracketIndex)
 
 	swapopen := make([]int, len(openBracketIndex))
 	copy(swapopen, openBracketIndex)
@@ -101,14 +102,14 @@ func ReplaceBrackets(query string) string {
 		swapclose[i] = closeBracketIndex[sortindex[i]]
 	}
 
-	fmt.Printf("Sorted open:\t%v\n", swapopen)
-	fmt.Printf("Sorted close:\t%v\n", swapclose)
+	log.Debugf("Sorted open:\t%v\n", swapopen)
+	log.Debugf("Sorted close:\t%v\n", swapclose)
 
 	returnquery := query
 
 	for i := range swapopen {
 		text := query[swapopen[i]:swapclose[i]]
-		fmt.Println(text)
+		log.Info(text)
 		for key, value := range mapper {
 			if strings.Contains(text, value) {
 				text = strings.ReplaceAll(text, value, key)
@@ -124,11 +125,13 @@ func ReplaceBrackets(query string) string {
 }
 
 func FormatSelect(query string) string {
-	query = CleanQuery(query)
-
 	// logger.Println("Formatting columns...")
 	// FORMAT COLUMNS
+	log.Debugf("Query under consideration:\n%v\n\n", query)
+	log.Debugf("Checking for columns in select:\t%v\n\n", selectcolumns.Match([]byte(query)))
+
 	columns := selectcolumns.FindStringSubmatch(query)[1]
+	log.Debugf("Columns:\n%v\n", columns)
 
 	columns = string(fmtcolumns.ReplaceAll([]byte(columns), []byte(",\n\t")))
 	columns = string(firstline.ReplaceAll([]byte(columns), []byte("\t$1")))
@@ -145,9 +148,12 @@ func FormatSelect(query string) string {
 		tables = table_li[1]
 	}
 
+	log.Info("Parsing from clause...")
 	tables = string(firstline.ReplaceAll([]byte(tables), []byte("")))
 	joins_li := fmttables.FindStringSubmatch(tables)
 	primary_table := joins_li[1]
+	log.Debugf("Primary table:%v\n", primary_table)
+	log.Debugf("Joins found:%v\n", joins_li)
 
 	returnquery = returnquery + "\nfrom\n\t" + primary_table
 
@@ -158,7 +164,7 @@ func FormatSelect(query string) string {
 		returnquery = returnquery + joins
 	}
 
-	// logger.Println("Formatting where clause...")
+	log.Info("Formatting where clause...")
 	// FORMAT WHERE
 	conditions := wherecond.FindStringSubmatch(query)
 	var conds string
@@ -166,7 +172,7 @@ func FormatSelect(query string) string {
 		where_li := wherecond2.FindStringSubmatch(query)
 		if len(where_li) > 0 {
 			for t := range where_li {
-				fmt.Printf("%v: %v\n", t, where_li[t])
+				log.Debugf("Conditions in WHERE:%v: %v\n", t, where_li[t])
 			}
 
 			conds = where_li[1]
@@ -176,7 +182,7 @@ func FormatSelect(query string) string {
 		}
 	} else {
 		for t := range conditions {
-			fmt.Printf("%v: %v\n", t, conditions[t])
+			log.Debugf("Conditions in WHERE:%v: %v\n", t, conditions[t])
 		}
 		conds = conditions[1]
 		conds = string(firstline.ReplaceAll([]byte(conds), []byte("")))
@@ -188,11 +194,63 @@ func FormatSelect(query string) string {
 }
 
 func FormatCTE(query string) string {
-	// cteselect := regexp.MustCompile(`\)\s(select.*)`)
-	//
-	// selectsmt := cteselect.FindString(query)
-	fmt.Printf("CTE Query:\n%v\n\n", query)
+	withre := regexp.MustCompile(`(?i)^(with\s[a-zA-Z0-9_]*\sas\s\()(.*?)(\))`)
+	nextctere := regexp.MustCompile(`(?i)(.*)(\),\s[a-zA-Z0-9_]*\sas\s\()(.*?)(\))`)
+	cteselect := regexp.MustCompile(`(?i)(\))\s(.*)`)
+
+	// log.Debugf("CTE Query:\n%v\n\n", query)
 	query = ReplaceBrackets(query)
 
-	return query
+	// match := nextctere.Match([]byte(query))
+	// log.Info(match)
+
+	returnquery := string(withre.ReplaceAll([]byte(query), []byte("$1\n\t$2\n$3")))
+	// log.Debugf("Formatted with:\n%v\n", returnquery)
+
+	returnquery = string(nextctere.ReplaceAll([]byte(returnquery), []byte("$1$2\n\t$3\n$4")))
+
+	// log.Debugf("Formatted with:\n%v\n", returnquery)
+
+	selectstmt := cteselect.FindStringSubmatch(returnquery)[2]
+	fmtselectstmt := FormatSelect(selectstmt)
+	log.Debugf("SELECT stmt in CTE:\n%v\n", fmtselectstmt)
+
+	returnquery = strings.ReplaceAll(returnquery, selectstmt, "")
+	log.Debugf("Query before sending for UNWRAP:\n%v\n", returnquery)
+
+	returnquery = unwrapbrackets(returnquery)
+	returnquery = returnquery + fmtselectstmt
+
+	return returnquery
+}
+
+func unwrapbrackets(query string) string {
+	log.Debugf("Unwrap func\nQuery:\n%v\n\n", query)
+	returnquery := query
+	match := true
+
+	for {
+		if !match {
+			break
+		}
+
+		for k, v := range mapper {
+			// log.Debugf("key:\t%v\nvalue:\t%v\n", k, v)
+			match = strings.Contains(returnquery, k)
+
+			if match {
+				log.Debugf("Matched %v in %v...\n", k, CleanQuery(returnquery))
+				q := matcher(CleanQuery(v))
+				log.Debugf("Formatted query in unwrap func:\n%v\n\n", q)
+
+				q = unwrapbrackets(q)
+				// match = false
+				// break
+				returnquery = strings.ReplaceAll(returnquery, k, q)
+				log.Debugf("Returned query after final unwrap\n%v\n", returnquery)
+			}
+		}
+	}
+
+	return returnquery
 }
